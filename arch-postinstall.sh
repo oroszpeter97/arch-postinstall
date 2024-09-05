dbd14a8510184fb34a6ff6eb403b9b8ed144cb06
p <github.p@proton.me>
2024-09-05 11:34:49 +0200

    confirmation

--- arch-postinstall.sh
+++ arch-postinstall.sh
@@ -3,14 +3,27 @@
 # update
 sudo pacman -Syu
 
+while true; do
+    read -p "Did you install your GPU drivers? y/n: " answer
+    if [[ "$answer" == "n" ]]; then
+        echo "Install them..."
+        exit 0
+    elif [[ "$answer" == "y" ]]; then
+        break
+    fi
+done
+
 # core
 sudo pacman -S --noconfirm vim mc git htop openssh pulseaudio pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack tree 
+
+#git config --global user.name "p"
+#git config --global user.email "github.p@proton.me"
+
 mkdir ~/sources ~/.config
 cd ~/sources
-git clone https://github.com/oroszpeter97/arch-postinstall.git
+#git clone https://github.com/oroszpeter97/arch-postinstall.git
 cd arch-postinstall
 
-pwd
 cp -r config-files/. ~/
 
 # yay
